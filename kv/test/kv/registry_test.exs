72376008fb968951e90d166f57bccd35c264ae84
ryonext <ryonext.s@gmail.com>
2015-10-07 22:39:00 +0900

    Add test

--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -22,4 +22,30 @@ defmodule KV.RegistryTest  do
     Agent.stop(bucket)
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
+
+  defmodule Forwarder do
+    use GenEvent
+
+    def handle_event(event, parent) do
+      send parent, event
+      {:ok, parent}
+    end
+  end
+
+  setup do
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = KV.Registry.start_link(manager)
+
+    GenEvent.add_mon_handler(manager, Forwarder, self())
+    {:ok, registry: registry}
+  end
+
+  test "sends events on create and crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.registry.lookup(registry, "shopping")
+    assert_receive {:create, "shopping", ^bucket}
+
+    Agent.stop(bucket)
+    assert_receive {:exit, "shopping", ^bucket}
+  end
 end
