778cfa584ba4029b7f193d9e4502563381ef13c1
ryonext <ryonext.s@gmail.com>
2015-10-18 23:22:45 +0900

    kv_server test works.

--- kv_umbrella/mix.exs
+++ kv_umbrella/mix.exs
@@ -2,24 +2,20 @@ defmodule KvUmbrella.Mixfile do
   use Mix.Project
 
   def project do
-    [apps_path: "apps",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :kv_server,
+      version: "0.0.1",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.0",
+      deps: deps]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
+  def application do
+    [applications: [:logger],
+      mod: {KVServer, []}]
+  end
+
   defp deps do
     []
   end
