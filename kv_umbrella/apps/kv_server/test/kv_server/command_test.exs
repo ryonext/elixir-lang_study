e294c13d05c4ea861a15e62d9945bc4cf1741758
ryonext <ryonext.s@gmail.com>
2015-10-25 21:51:08 +0900

    Add tests.

--- kv_umbrella/apps/kv_server/test/kv_server/command_test.exs
+++ kv_umbrella/apps/kv_server/test/kv_server/command_test.exs
@@ -1,4 +1,34 @@
 defmodule KVServer.CommandTest do
   use ExUnit.Case, async: true
   doctest KVServer.Command
+@doc ~S"""
+Parses the given `line` into a command.
+
+## Examples
+
+    iex> KVServer.Command.parse "CREATE shopping\r\n"
+    {:ok, {:create, "shopping"}}
+
+    iex> KVServer.Command.parse "CREATE  shopping  \r\n"
+    {:ok, {:create, "shopping"}}
+
+    iex> KVServer.Command.parse "PUT shopping milk 1\r\n"
+    {:ok, {:put, "shopping", "milk", "1"}}
+
+    iex> KVServer.Command.parse "GET shopping milk\r\n"
+    {:ok, {:get, "shopping", "milk"}}
+
+    iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
+    {:ok, {:delete, "shopping", "eggs"}}
+
+Unknown commands or commands with the wrong number of
+arguments return an error:
+
+    iex> KVServer.Command.parse "UNKNOWN shopping eggs\r\n"
+    {:error, :unknown_command}
+
+    iex> KVServer.Command.parse "GET shopping\r\n"
+    {:error, :unknown_command}
+
+"""
 end
