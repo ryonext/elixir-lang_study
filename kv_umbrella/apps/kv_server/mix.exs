7b585bd4c6b386a0dfcfb797e767bf4c7d86d96e
ryonext <ryonext.s@gmail.com>
2015-10-29 20:12:47 +0900

    Add kv.

--- kv_umbrella/apps/kv_server/mix.exs
+++ kv_umbrella/apps/kv_server/mix.exs
@@ -16,7 +16,7 @@ defmodule KVServer.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :kv, :pipe],
+    [applications: [:logger, :pipe, :kv],
      mod: {KVServer, []}]
   end
 
@@ -34,6 +34,7 @@ defmodule KVServer.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:pipe, github: "batate/elixir-pipes"}]
+    [{:kv, in_umbrella: true},
+      {:pipe, github: "batate/elixir-pipes"}]
   end
 end
