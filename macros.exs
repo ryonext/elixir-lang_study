46e94830af37b8aa41eaa53a466ce3242a9514db
ryonext <ryonext.s@gmail.com>
2015-11-02 19:40:42 +0900

    Macros hygiene

--- macros.exs
+++ macros.exs
@@ -15,3 +15,41 @@ defmodule Unless do
     end
   end
 end
+
+defmodule Hygiene do
+  defmacro no_interference do
+    quote do: var!(a) = 1
+  end
+end
+
+defmodule HygieneTest do
+  def go do
+    require Hygiene
+    a = 13
+    Hygiene.no_interference
+    a
+  end
+end
+
+defmodule Sample do
+  def quoted do
+    quote do: x
+  end
+end
+
+defmodule Sample do
+  defmacro initialize_to_char_count(variables) do
+    Enum.map variables, fn(name) ->
+      var = Macro.var(name, nil)
+      length = name |> Atom.to_string |> String.length
+      quote do
+        unquote(var) = unquote(length)
+      end
+    end
+  end
+
+  def run do
+    initialize_to_char_count [:red, :green, :yellow]
+    [red, green, yellow]
+  end
+end
